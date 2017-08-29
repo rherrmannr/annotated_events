6a0ba9d779b41f37eb0717036f676497ad3b8f3d
rherrmannr <Robin.Herrmann@itemis.com>
2017-08-29 14:50:58 +0200

    Update ExampleInterruptHandler.c

--- ExampleInterruptHandler.c
+++ ExampleInterruptHandler.c
@@ -1,12 +1,3 @@
-/* 
- * Platform interrupt support
- * Manufactor:		Atmel
- * Derivate:		NONE
- * Family:			NONE
- * Name:			ATMmega
- * Architecture:	NONE
-*/
-
 #include "ExampleInterruptHandler.h"
 
 InterruptMem im;
@@ -17,26 +8,12 @@ void example_InterruptHandler(Example* handle){
 			exampleIface_raise_adc(handle);
 			im.vector_flag = 0; 
 			break;
-		}
-		
+		}		
 		case INT0_vect_num: {
 			exampleIface_raise_button1(handle);
 			im.vector_flag = 0; 
 			break;
-		}
-		
-		case INT1_vect_num: {
-			exampleIface_raise_button2(handle);
-			im.vector_flag = 0; 
-			break;
-		}
-		
-		case TIMER1_COMPA_vect_num: {
-			exampleIface_raise_timer1(handle);
-			im.vector_flag = 0; 
-			break;
-		}
-		
+		}	
 		default: {break;}
 	}
 }
@@ -45,15 +22,8 @@ ISR (ADC_vect){
 	im.vector_flag = ADC_vect_num;
 	example_ADC_vect_num_ISR(&im);
 }
+
 ISR (INT0_vect){
 	im.vector_flag = INT0_vect_num;
 	example_INT0_vect_num_ISR(&im);
-}
-ISR (INT1_vect){
-	im.vector_flag = INT1_vect_num;
-	example_INT1_vect_num_ISR(&im);
-}
-ISR (TIMER1_COMPA_vect){
-	im.vector_flag = TIMER1_COMPA_vect_num;
-	example_TIMER1_COMPA_vect_num_ISR(&im);
 }
