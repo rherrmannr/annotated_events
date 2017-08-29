87524dbbcea9c95e234407e7e2e2dc4d7ec6b27b
rherrmannr <Robin.Herrmann@itemis.com>
2017-08-29 14:41:53 +0200

    Add files via upload

---
+++ MSA_ARD_90_manTestbeispiel.ino
@@ -0,0 +1,26 @@
+#include "Arduino.h"
+#include "ManualExample.h"
+#include "src-gen/ManualExampleInterruptHandler.h"
+#include "avr/sleep.h"
+
+
+ManualExample sc;
+
+void setup()
+{
+	set_sleep_mode(SLEEP_MODE_IDLE);
+	sleep_enable();
+	manualExample_init(&sc);
+	manualExample_enter(&sc);
+	manualExample_runCycle(&sc);
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+	manualExample_InterruptHandler(&sc);
+	manualExample_runCycle(&sc);
+	interrupts();
+	sleep_cpu();
+	noInterrupts();
+}
